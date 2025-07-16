46933d01cdcda0bdc574aaec843b2fe284e94d64
Letícia Gomes <gleticia236@gmail.com>
2025-07-16 09:19:37 -0300

    fix: user sesion path

--- octavus/test/services/usersessionservice_test.dart
+++ octavus/test/services/usersessionservice_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:octavus/services/auth/user_session_service.dart';
+import 'package:octavus/services/Auth/user_session_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
